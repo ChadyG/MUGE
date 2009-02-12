57cc59b7d161548b7fe34428798099557732902c
pandagoat <pandagoat@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-02-12 20:15:11 +0000

    I fixed some warnings during compilation of JSONFile.cpp.  Also, I made it so JSONFile can handle arrays.  If you have an array with an object in it, all in another object, you can do get("object1.array[0].someMember").  And if you have an array of arrays, you can use [][]: get("object1.array[0][3]").

--- src/Engine/JSONFile.cpp
+++ src/Engine/JSONFile.cpp
@@ -19,10 +19,11 @@ void JSONFile::readFile(std::string fileName)
 void JSONFile::parseContent()
 {
      this->variant = json::parse(this->fileContent.begin(), this->fileContent.end());
-	
+
      // If this.variant is not of the type
      // json::grammar<char>::object, then there was an error.
-     if(this->variant->type() != typeid(json::grammar<char>::object)) 
+  
+   if(this->variant->type() != typeid(json::grammar<char>::object)) 
      {
 	  // do something to handle the error
 	  return; 		
@@ -30,25 +31,3 @@ void JSONFile::parseContent()
 	
      this->config = boost::any_cast<json::grammar<char>::object>(*this->variant);	
 }
-/*
-template <class T>
-T JSONFile::get(std::string name, 
-		const boost::shared_ptr<boost::any> object)
-{
-     int index = name.find(".", 0);
-     
-     json::grammar<char>::object o = object ? boost::any_cast<json::grammar<char>::object>(*object) : this->config;
-  
-     if (index != std::string::npos)
-     {
-	  return get<T>(name.substr(index+1), o[name.substr(0, index)]);    
-     } 
-     else 
-     {
-	  if(o[name])
-	       return boost::any_cast<T>(*o[name]);
-     }
-  
-     return T();
-}
-*/
--- src/Engine/JSONFile.hpp
+++ src/Engine/JSONFile.hpp
@@ -1,15 +1,14 @@
 #ifndef JSONFILE_H
 #define JSONFILE_H
 
-#include "tinyjson/tinyjson.hpp"
+#include "tinyjson.hpp"
 #include <string>
-
 
 class JSONFile
 {
 public:
      JSONFile(std::string fileName);
-
+     
      // -------------------------------------------------------------
      // The purpose of splitting up parsing a file into seperate
      // methods (readFile() and parseContent()) is to allow dynamic
@@ -47,10 +46,9 @@ public:
       * type for the template should be the expected type of the
       * variable.
       **/
-      
      template <class T>
      T get(std::string name, 
-	    const boost::shared_ptr<boost::any> object = boost::shared_ptr<boost::any>());
+	   const boost::shared_ptr<boost::any> object = boost::shared_ptr<boost::any>());
 
      
      
@@ -59,27 +57,88 @@ private:
      json::grammar<char>::object config; 
      std::string fileName;
      std::string fileContent;
+
+     template <class T>
+     T getArrayIndex(boost::shared_ptr<boost::any> a, unsigned int index);
 };
 
+
+// ------------------------------------------------------------------
 template <class T>
 T JSONFile::get(std::string name, 
 		const boost::shared_ptr<boost::any> object)
 {
-     int index = name.find(".", 0);
+     unsigned int index = name.find(".", 0);
      
      json::grammar<char>::object o = object ? boost::any_cast<json::grammar<char>::object>(*object) : this->config;
   
+     // The variable "name" still contains an object
      if (index != std::string::npos)
-     {
-	  return get<T>(name.substr(index+1), o[name.substr(0, index)]);    
+     {				
+	  return get<T>(name.substr(index+1), get<boost::shared_ptr<boost::any> >(name.substr(0, index), object));
      } 
+     // "object" is now the direct parent of the variable "name"
      else 
      {
+	  // check to see if the variable we want is an array by
+	  // seeing if it has [] in the name	  
+	  unsigned int arrayOperatorIndex = name.find("[", 0);
+	  unsigned int arrayIndex = 0;
+	  boost::shared_ptr<boost::any> var = boost::shared_ptr<boost::any>();
+		
+
+	  if(arrayOperatorIndex != std::string::npos)
+	       var = o[name.substr(0, arrayOperatorIndex)];
+	           
+	  while(arrayOperatorIndex != std::string::npos && var)
+	  {
+	       arrayIndex = boost::lexical_cast<int>(name.substr(arrayOperatorIndex + 1, 
+								 name.find("]", arrayOperatorIndex) - 
+								 arrayOperatorIndex - 1));
+	       
+	       arrayOperatorIndex = name.find("[", arrayOperatorIndex+1);
+
+	       
+	       // no more []
+	       if(var && arrayOperatorIndex == std::string::npos)
+	       {
+		    return getArrayIndex<T>(var, arrayIndex);
+	       }
+	       // more []
+	       else
+	       {
+		    var = getArrayIndex< boost::shared_ptr<boost::any> >(var, arrayIndex);
+	       }	       
+	  }
 	  if(o[name])
 	       return boost::any_cast<T>(*o[name]);
      }
   
      return T();
+}
+
+
+// ------------------------------------------------------------------
+template <class T>
+T JSONFile::getArrayIndex(boost::shared_ptr<boost::any> var, unsigned int index)
+{
+
+     json::grammar<char>::array const & a = boost::any_cast< json::grammar<char>::array >(*var);
+     
+     int i = 0;
+     for(json::grammar<char>::array::const_iterator it = a.begin(); it != a.end(); ++it, ++i)
+     {
+	  
+	  if(i == index)
+	  {
+	       // if we want to return a boost:shared_ptr, then
+	       // dereferencing it twice will screw up
+	       if(typeid(T) == typeid(boost::shared_ptr<boost::any>))
+		    return boost::any_cast<T>(*it);
+	       else
+		    return boost::any_cast<T>(**it);
+	  }
+     }
 }
 
 #endif
